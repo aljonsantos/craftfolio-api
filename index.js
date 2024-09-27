72a709b3e534a8344644978ad0e871cf6db53eb8
aljonsantos <cs.aljonssantos@gmail.com>
2024-09-27 14:52:42 +0800

    setup static file serving

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+app.use(express.static('dist'));
 app.use(express.json());
 
 app.get('/ping', (req, res) => {
