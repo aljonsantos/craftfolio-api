ddd49556c3f7be8d60fba4449a304766c1181605
aljonsantos <cs.aljonssantos@gmail.com>
2024-10-15 20:58:57 +0800

    frontend code sync commit -m 09378684

--- utils/dynamicFiles.js
+++ utils/dynamicFiles.js
@@ -386,11 +386,11 @@ const generateNavbarFile = (content) => {
   let links
   if (content.page === 'single') {
     links = `enabledPages.map(
-      page => <Fade key={page}><li><a href="" data-nav-section={page} onClick={handleClick} className={\`\${page === activePage ? 'active': ''} px-[1em] py-[.8em] inline-block rounded-full m-[1px] transition-all duration-500 hover:bg-accent-200 hover:text-accent-800 hover:font-semibold\`}>{page}</a></li></Fade>
+      page => <li key={page}><a href="" data-nav-section={page} onClick={handleClick} className={\`\${page === activePage ? 'active': ''} px-[1em] py-[.8em] inline-block rounded-full m-[1px] transition-all duration-500 hover:bg-accent-200/20 hover:text-accent-800 hover:font-semibold\`}>{page}</a></li>
     )`
   } else if (content.page === 'multi') {
     links = `enabledPages.map(
-      page => <Fade key={page}><li><Link to={page} data-nav-section={page} onClick={handleClick} className={\`\${page === activePage ? 'active': ''} px-[1em] py-[.8em] inline-block rounded-full m-[1px] transition-all duration-500 hover:bg-accent-200 hover:text-accent-800 hover:font-semibold\`}>{page}</Link></li></Fade>
+      page => <li key={page}><Link to={page} data-nav-section={page} onClick={handleClick} className={\`\${page === activePage ? 'active': ''} px-[1em] py-[.8em] inline-block rounded-full m-[1px] transition-all duration-500 hover:bg-accent-200/20 hover:text-accent-800 hover:font-semibold\`}>{page}</Link></li>
     )`
   }
 
@@ -406,6 +406,7 @@ const generateNavbarFile = (content) => {
       top: targetOffset - offset,
       behavior: 'smooth'
     })
+    setIsAutoScrolling(true)
     `
   )
 
@@ -430,7 +431,13 @@ const generateNavbarFile = (content) => {
           }
         })
   
-        setActivePage(activeSection)
+        if (isAutoScrolling) {
+          if (activeSection === activePage) {
+            setIsAutoScrolling(false)
+          }
+        } else {
+          setActivePage(activeSection)
+        }
       }
   
       window.addEventListener('scroll', handleScroll)
@@ -439,12 +446,13 @@ const generateNavbarFile = (content) => {
       return () => {
         window.removeEventListener('scroll', handleScroll)
       }
-    }, [setActivePage, enabledPages])
+    }, [setActivePage, enabledPages, isAutoScrolling, activePage])
     `
   )
 
-  return `import { useEffect, useContext } from 'react'
+  return `import { useState, useEffect, useContext } from 'react'
 ${content.page === 'multi' ? "import { Link } from 'react-router-dom'" : ''}
+import { motion } from 'framer-motion'
 import AppContext from '../contexts/AppContext'
 import ThemeContext from '../contexts/ThemeContext'
 import { IconContrast } from './Icons'
@@ -454,9 +462,29 @@ const Navbar = () => {
   const { activePage, setActivePage } = useContext(AppContext)
   const { toggleTheme } = useContext(ThemeContext)
   const enabledPages = ${JSON.stringify(enabledPages)}
+  ${content.page === 'single' ? 'const [isAutoScrolling, setIsAutoScrolling] = useState(false)' : ''}
+
+  useEffect(() => {
+    handleActivePageLinkPos(activePage)
+  }, [activePage])
+
+  const [navLinkPos, setNavLinkPos] = useState({
+    left: 0,
+    width: 0,
+  })
+
+   const handleActivePageLinkPos = (newActivePage) => {    
+    const activeLink = document.querySelector(\`[data-nav-section="\${newActivePage}"]\`)
+    if (activeLink) {
+      setNavLinkPos({
+        width: activeLink.getBoundingClientRect().width - 2,
+        left: activeLink.offsetLeft + 1,
+      })
+    }
+  }
 
   const handleClick = (e) => {
-    ${content.page === 'single' ? scrollToSection : ''}
+    ${content.page === 'single' ? scrollToSection : 'window.scrollTo({ top: 0 })'}
     setActivePage(e.target.dataset.navSection)
   }
 
@@ -466,7 +494,14 @@ const Navbar = () => {
   return (
     <nav className="navbar fixed md:sticky w-full bottom-[24px] lg:bottom-[50px] left-0 md:top-[70px] flex justify-center items-center z-50 lg:mb-6">
       <ul className="flex border capitalize bg-background-700/10 backdrop-blur-xl backdrop-saturate-150 md:bg-background-700 text-[13px] md:text-[14px] text-accent-800 border-accent-100 rounded-3xl shadow-lg lg:shadow-xl lg:hover:scale-105 lg:active:scale-100 transition-transform duration-500">
-        {links}
+        <Fade props={{ className: 'flex relative '}}>
+          {links}
+          <motion.li
+            className="active-pill absolute bg-accent-200 rounded-full h-[calc(100%-4px)] top-1/2 -translate-y-1/2 -z-10" 
+            animate={navLinkPos}
+            initial={false}
+          />
+        </Fade>
       </ul>
       <button className='border p-2 md:p-[10px] ml-3 md:ml-4 rounded-full bg-background-700/10 backdrop-blur-xl backdrop-saturate-150 md:bg-background-700 text-[13px] md:text-[14px] text-content-700 border-border/10 shadow-lg lg:shadow-xl lg:hover:scale-105 lg:active:scale-100' onClick={toggleTheme}>
         <Fade>
@@ -610,7 +645,7 @@ a, button, input, textarea {
 }
 
 .navbar .active {
-  @apply font-semibold text-accent-800 bg-accent-200;
+  @apply font-semibold text-accent-800;
 }
 
 .main {
